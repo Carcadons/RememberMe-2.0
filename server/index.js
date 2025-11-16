175c3053cd9efdf0a67e44d7a9f2575df6ab0f7d
Francesco Tizzani <francesco@tizzani.org>
2025-11-16 14:58:58 +0000

    Replit fixes

--- server/index.js
+++ server/index.js
@@ -312,7 +312,7 @@ function startServer() {
 
   // === START SERVER ===
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, '0.0.0.0', () => {
     console.log('[ServerV2] ====== SERVER RUNNING ======');
     console.log(`[ServerV2] Port: ${PORT}`);
